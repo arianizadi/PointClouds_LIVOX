781a50ed2b27a4a3701ee311d7c1465dfe44e804
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-08-22 14:43:38 -0700

    Modernize

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 #include <arpa/inet.h>
 #include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <pcl/common/common.h>
@@ -12,9 +15,6 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 pcl::PointCloud< pcl::PointXYZ >::Ptr
@@ -22,22 +22,22 @@ pcl::PointCloud< pcl::PointXYZ >::Ptr
 int counter = 0;
 const int MIN_POINTS = 1000; // Adjust this value based on your needs
 
-void PointCloudCallback(uint32_t handle,
-                        const uint8_t dev_type,
+void PointCloudCallback(uint32_t /*handle*/,
+                        const uint8_t /*dev_type*/,
                         LivoxLidarEthernetPacket *data,
-                        void *client_data) {
+                        void * /*client_data*/) {
   if(data == nullptr) {
     return;
   }
 
-  LivoxLidarCartesianHighRawPoint *p_point_data
-      = (LivoxLidarCartesianHighRawPoint *) data->data;
+  auto *p_point_data
+      = reinterpret_cast< LivoxLidarCartesianHighRawPoint * >(data->data);
 
   for(uint32_t i = 0; i < data->dot_num; ++i) {
     pcl::PointXYZ point;
-    point.x = p_point_data[i].x / 1000.0;
-    point.y = p_point_data[i].y / 1000.0;
-    point.z = p_point_data[i].z / 1000.0;
+    point.x = static_cast< float >(p_point_data[i].x) / 1000.0F;
+    point.y = static_cast< float >(p_point_data[i].y) / 1000.0F;
+    point.z = static_cast< float >(p_point_data[i].z) / 1000.0F;
     cloud->push_back(point);
   }
   counter += data->dot_num;
@@ -46,45 +46,40 @@ void PointCloudCallback(uint32_t handle,
 void WorkModeCallback(livox_status status,
                       uint32_t handle,
                       LivoxLidarAsyncControlResponse *response,
-                      void *client_data) {
+                      void * /*client_data*/) {
   if(response == nullptr) {
     return;
   }
-  printf(
-      "WorkModeCallack, status:%u, handle:%u, ret_code:%u, error_key:%u",
-      status,
-      handle,
-      response->ret_code,
-      response->error_key);
+  std::cout << "WorkModeCallack, status:" << status
+            << ", handle:" << handle
+            << ", ret_code:" << response->ret_code
+            << ", error_key:" << response->error_key << std::endl;
 }
 
 void RebootCallback(livox_status status,
                     uint32_t handle,
                     LivoxLidarRebootResponse *response,
-                    void *client_data) {
+                    void * /*client_data*/) {
   if(response == nullptr) {
     return;
   }
-  printf("RebootCallback, status:%u, handle:%u, ret_code:%u",
-         status,
-         handle,
-         response->ret_code);
+  std::cout << "RebootCallback, status:" << status
+            << ", handle:" << handle
+            << ", ret_code:" << response->ret_code << std::endl;
 }
 
 void SetIpInfoCallback(livox_status status,
                        uint32_t handle,
                        LivoxLidarAsyncControlResponse *response,
-                       void *client_data) {
+                       void * /*client_data*/) {
   if(response == nullptr) {
     return;
   }
-  printf(
-      "LivoxLidarIpInfoCallback, status:%u, handle:%u, ret_code:%u, "
-      "error_key:%u",
-      status,
-      handle,
-      response->ret_code,
-      response->error_key);
+
+  std::cout << "LivoxLidarIpInfoCallback, status:" << status
+            << ", handle:" << handle
+            << ", ret_code:" << response->ret_code
+            << ", error_key:" << response->error_key << std::endl;
 
   if(response->ret_code == 0 && response->error_key == 0) {
     LivoxLidarRequestReboot(handle, RebootCallback, nullptr);
@@ -95,9 +90,9 @@ void QueryInternalInfoCallback(
     livox_status status,
     uint32_t handle,
     LivoxLidarDiagInternalInfoResponse *response,
-    void *client_data) {
+    void * /*client_data*/) {
   if(status != kLivoxLidarStatusSuccess) {
-    printf("Query lidar internal info failed.\n");
+    std::cout << "Query lidar internal info failed.\n";
     QueryLivoxLidarInternalInfo(
         handle, QueryInternalInfoCallback, nullptr);
     return;
@@ -107,62 +102,56 @@ void QueryInternalInfoCallback(
     return;
   }
 
-  uint8_t host_point_ipaddr[4] {0};
+  std::array< uint8_t, 4 > host_point_ipaddr {0};
   uint16_t host_point_port = 0;
   uint16_t lidar_point_port = 0;
 
-  uint8_t host_imu_ipaddr[4] {0};
+  std::array< uint8_t, 4 > host_imu_ipaddr {0};
   uint16_t host_imu_data_port = 0;
   uint16_t lidar_imu_data_port = 0;
 
   uint16_t off = 0;
-  for(uint8_t i = 0; i < response->param_num; ++i) {
-    LivoxLidarKeyValueParam *kv
-        = (LivoxLidarKeyValueParam *) &response->data[off];
+  for(uint16_t i = 0; i < response->param_num; ++i) {
+    auto *kv = reinterpret_cast< LivoxLidarKeyValueParam * >(
+        &response->data[off]);
     if(kv->key == kKeyLidarPointDataHostIpCfg) {
-      memcpy(host_point_ipaddr, &(kv->value[0]), sizeof(uint8_t) * 4);
-      memcpy(&(host_point_port), &(kv->value[4]), sizeof(uint16_t));
-      memcpy(&(lidar_point_port), &(kv->value[6]), sizeof(uint16_t));
+      std::copy_n(&kv->value[0], 4, host_point_ipaddr.begin());
+      std::memcpy(&host_point_port, &kv->value[4], sizeof(uint16_t));
+      std::memcpy(&lidar_point_port, &kv->value[6], sizeof(uint16_t));
     } else if(kv->key == kKeyLidarImuHostIpCfg) {
-      memcpy(host_imu_ipaddr, &(kv->value[0]), sizeof(uint8_t) * 4);
-      memcpy(&(host_imu_data_port), &(kv->value[4]), sizeof(uint16_t));
-      memcpy(&(lidar_imu_data_port), &(kv->value[6]), sizeof(uint16_t));
+      std::copy_n(&kv->value[0], 4, host_imu_ipaddr.begin());
+      std::memcpy(&host_imu_data_port, &kv->value[4], sizeof(uint16_t));
+      std::memcpy(&lidar_imu_data_port, &kv->value[6], sizeof(uint16_t));
     }
-    off += sizeof(uint16_t) * 2;
-    off += kv->length;
+    off += sizeof(uint16_t) * 2 + kv->length;
   }
 
-  printf(
-      "Host point cloud ip addr:%u.%u.%u.%u, host point cloud port:%u, "
-      "lidar point cloud port:%u.\n",
-      host_point_ipaddr[0],
-      host_point_ipaddr[1],
-      host_point_ipaddr[2],
-      host_point_ipaddr[3],
-      host_point_port,
-      lidar_point_port);
+  std::cout << "Host point cloud ip addr:" << host_point_ipaddr[0] << '.'
+            << host_point_ipaddr[1] << '.' << host_point_ipaddr[2] << '.'
+            << host_point_ipaddr[3]
+            << ", host point cloud port:" << host_point_port
+            << ", lidar point cloud port:" << lidar_point_port
+            << std::endl;
 
-  printf(
-      "Host imu ip addr:%u.%u.%u.%u, host imu port:%u, lidar imu "
-      "port:%u.\n",
-      host_imu_ipaddr[0],
-      host_imu_ipaddr[1],
-      host_imu_ipaddr[2],
-      host_imu_ipaddr[3],
-      host_imu_data_port,
-      lidar_imu_data_port);
+  // Output IMU info
+  std::cout << "Host imu ip addr:" << host_imu_ipaddr[0] << '.'
+            << host_imu_ipaddr[1] << '.' << host_imu_ipaddr[2] << '.'
+            << host_imu_ipaddr[3]
+            << ", host imu port:" << host_imu_data_port
+            << ", lidar imu port:" << lidar_imu_data_port << std::endl;
 }
 
 void LidarInfoChangeCallback(const uint32_t handle,
                              const LivoxLidarInfo *info,
-                             void *client_data) {
+                             void * /*client_data*/) {
   if(info == nullptr) {
-    printf("lidar info change callback failed, the info is nullptr.\n");
+    std::cerr << "lidar info change callback failed, the info is nullptr."
+              << std::endl;
     return;
   }
-  printf("LidarInfoChangeCallback Lidar handle: %u SN: %s\n",
-         handle,
-         info->sn);
+  std::cout << "LidarInfoChangeCallback Lidar handle: " << handle
+            << " SN: " << static_cast< const char * >(info->sn)
+            << std::endl;
 
   SetLivoxLidarWorkMode(
       handle, kLivoxLidarNormal, WorkModeCallback, nullptr);
